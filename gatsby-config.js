ef3839ee4981735c5e44a504f764ec84757edf14
seele123o <xuchangchun77@Gmail.com>
2024-10-07 07:21:29 +0800

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: 'V2Board',
+    description: 'V2Board 前端',
+    keywords: ['V2Board', '前端'],
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-manifest',
+  ],
+};
