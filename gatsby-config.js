0d26fc1e8b909ec59f83a6977a7d4d574258b099
seele123o <xuchangchun77@Gmail.com>
2024-10-07 07:27:54 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,4 @@ module.exports = {
     description: 'V2Board 前端',
     keywords: ['V2Board', '前端'],
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-manifest',
-  ],
-};
+
