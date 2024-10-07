e1d5575ed7b507922187addb9639445f188df1e1
seele123o <xuchangchun77@Gmail.com>
2024-10-07 08:09:03 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ const config: DefaultConfigProps = {
   background_url: "",
   description: "hahaha",
   logo: "logourl",
-  api: "seele574939913",
+  api: "http://cc.byegirls.com",
   languages: ["en-US","zh-CN","zh-TW"],
   emojiEndpoint: "https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/{{code}}.png",
   startYear: 2021
